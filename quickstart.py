48bfa2569fa8640408f4f2ee390a615dc91fd819
tydunn <t.dunn@rasa.com>
2022-06-15 14:22:19 +0200

    tweaking first version

--- quickstart.py
+++ quickstart.py
@@ -53,10 +53,10 @@ f = open(data_schema_file_path, "a")
 f.write(schema_yaml)
 f.close()
 
-# Load
+# 9a. Load
 pipeline.load()
 
-# 5. Optional error handling - print, raise or handle.
+# 9b. Optional error handling - print, raise or handle.
 
 # now enumerate all complete loads if we have any failed packages
 # complete but failed job will not raise any exceptions
@@ -67,32 +67,20 @@ for load_id in completed_loads:
     for job, failed_message in pipeline.list_failed_jobs(load_id):
         print(f"JOB: {job}\nMSG: {failed_message}")
 
-# now you can use your data
-
-# Tables created:
-
+# 10a. Run SQL queries
 with pipeline.sql_client() as c:
+    
     query = f"SELECT * FROM `{schema_prefix}_example.my_json_doc`"
     df = c._execute_sql(query)
     print(f"SELECT * FROM `{schema_prefix}_example.my_json_doc`")
     print(list(df))
     print()
-    # {  "name": "Ana",  "age": "30",  "id": "456",  "_load_id": "1654787700.406905",  "_record_hash": "5b018c1ba3364279a0ca1a231fbd8d90"}
-    # {  "name": "Bob",  "age": "30",  "id": "455",  "_load_id": "1654787700.406905",  "_record_hash": "afc8506472a14a529bf3e6ebba3e0a9e"}
 
     query = f"SELECT * FROM `{schema_prefix}_example.my_json_doc__children` LIMIT 1000"
     df = c._execute_sql(query)
     print(query)
     print(list(df))
     print()
-    # {"name": "Bill", "id": "625", "_parent_hash": "5b018c1ba3364279a0ca1a231fbd8d90", "_pos": "0", "_root_hash": "5b018c1ba3364279a0ca1a231fbd8d90",
-    #   "_record_hash": "7993452627a98814cc7091f2c51faf5c"}
-    # {"name": "Bill", "id": "625", "_parent_hash": "afc8506472a14a529bf3e6ebba3e0a9e", "_pos": "0", "_root_hash": "afc8506472a14a529bf3e6ebba3e0a9e",
-    #   "_record_hash": "9a2fd144227e70e3aa09467e2358f934"}
-    # {"name": "Dave", "id": "621", "_parent_hash": "afc8506472a14a529bf3e6ebba3e0a9e", "_pos": "1", "_root_hash": "afc8506472a14a529bf3e6ebba3e0a9e",
-    #   "_record_hash": "28002ed6792470ea8caf2d6b6393b4f9"}
-    # {"name": "Elli", "id": "591", "_parent_hash": "5b018c1ba3364279a0ca1a231fbd8d90", "_pos": "1", "_root_hash": "5b018c1ba3364279a0ca1a231fbd8d90",
-    #   "_record_hash": "d18172353fba1a492c739a7789a786cf"}
 
     # and we can join them via auto generated keys
     query = f"""
@@ -104,7 +92,3 @@ with pipeline.sql_client() as c:
     """
     df = c._execute_sql(query)
     print(list(df))
-    # {  "name": "Ana",  "age": "30",  "parent_id": "456",  "child_name": "Bill",  "child_id": "625",  "child_order_in_list": "0"}
-    # {  "name": "Ana",  "age": "30",  "parent_id": "456",  "child_name": "Elli",  "child_id": "591",  "child_order_in_list": "1"}
-    # {  "name": "Bob",  "age": "30",  "parent_id": "455",  "child_name": "Bill",  "child_id": "625",  "child_order_in_list": "0"}
-    # {  "name": "Bob",  "age": "30",  "parent_id": "455",  "child_name": "Dave",  "child_id": "621",  "child_order_in_list": "1"}
